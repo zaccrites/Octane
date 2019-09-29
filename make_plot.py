90c36d91ca0d12652702725bf9f21da96f1a92fb
Zac Crites <zac@criteshouse.com>
2019-09-29 13:25:57 -0400

    Add FFT to plot output

--- make_plot.py
+++ make_plot.py
@@ -2,77 +2,68 @@
 import sys
 import csv
 
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft
 
 
 def main():
 
-    series_names = None
     x = []
     samples = []
-    # op1_output = []
-    # op2_output = []
 
+    SAMPLE_FREQ = 44100
 
     with open('data.csv', 'r') as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
-            if i == 0:
-                series_names = row
-            else:
-                MAX_VALUE = 2**(16 - 1)
-                SAMPLE_FREQ = 44100
+            MAX_VALUE = 2**(16 - 1)
+            x.append(int(row[0]))
+            samples.append(int(row[1]) / MAX_VALUE)
 
-                # x.append(int(row[0]) / SAMPLE_FREQ)
-                x.append(int(row[0]))
-                samples.append(int(row[1]) / MAX_VALUE)
+    # https://stackoverflow.com/a/23378284
+    sample_fft = fft(samples)
+    d = len(sample_fft) // 2
+    yf = abs(sample_fft[:(d-1)])
+    #
+    k = np.arange(d - 1)
+    T = len(yf) / SAMPLE_FREQ
+    freq_labels = k / T
 
-                # op1_output.append(int(row[2]) / MAX_VALUE)
-                # op2_output.append(int(row[3]) / MAX_VALUE)
+    # TODO: Clean this up. I had to add this line in order for the FFT
+    # spikes at known frequencies to line up correctly.
+    freq_labels = freq_labels / 2
 
-
-    # This is extremely crude. It would be cool to do a Fourier transform
-    # on the output instead to get the frequency components
-    # (especially to debug operator combinations)
-    # Having this output on the real keyboard would be cool too.
-    zeroes = []
-    for i, (xi, samplei) in enumerate(zip(x, samples)):
-        if i > 0 and samples[i - 1] < 0 and samplei > 0:
-            zeroes.append((xi / SAMPLE_FREQ, samplei))
-    if zeroes:
-        x_range = max([zero[0] for zero in zeroes]) - min([zero[0] for zero in zeroes])
-        # y_range = max([zero[1] for zero in zeroes]) - min([zero[1] for zero in zeroes])
-        print(f'VERY approximate frequency is {len(zeroes) / x_range} Hz')
-        print(f'Num zeroes: {len(zeroes)}')
-    else:
-        print('No zeroes found')
+    # Normalize output for relative freuqency contributions to overall spectrum
+    yf = yf / max(yf)
 
 
-    # plt.plot(x, samples, x, op1_output, x, op2_output)
-
-    # https://matplotlib.org/3.1.0/gallery/subplots_axes_and_figures/subplots_demo.html
-    # fig, axes = plt.subplots(2)
-    # axes[0].set_title('Output Sample')
-    # axes[0].plot(x, samples)
-    # # axes[1].set_title('Operator 1 Output')
-    # # axes[1].plot(x, op1_output, 'tab:orange')
-    # # axes[2].set_title('Operator 2 Output')
-    # # axes[2].plot(x, op2_output, 'tab:green')
-
-    # for ax in axes:
-    #     ax.label_outer()
-    #     ax.grid(True)
-
-    fig = plt.figure()
-    plt.plot(x, samples)
-    # plt.suptitle('Output')
-    plt.grid(True)
+    fig, axes = plt.subplots(2, figsize=(10, 10))
+    #
+    NUM_OUTPUT_SAMPLES = 1000
+    axes[0].set_title('Samples')
+    axes[0].plot(x[:NUM_OUTPUT_SAMPLES], samples[:NUM_OUTPUT_SAMPLES])
+    axes[0].set_xlabel('Sample Number')
+    axes[0].set_ylabel('Amplitude')
+    #
+    axes[1].set_title('FFT')
+    axes[1].plot(freq_labels, yf, 'r')
+    axes[1].set_xlim([0, 1000])
+    axes[1].set_xlabel('Frequency [Hz]')
+    axes[1].set_ylabel('Relative Strength')
+    #
+    for ax in axes:
+        ax.grid(True)
+    #
+    plt.suptitle('Synthesizer Output')
     plt.savefig('data.png')
-
 
     print(f'Min: {min(samples):.04f}')
     print(f'Max: {max(samples):.04f}')
 
+    # peak frequency from FFT
+    peak_freq = max(zip(yf, freq_labels))
+    print(f'{peak_freq[1]:.02f} Hz')
 
 
 if __name__ == '__main__':
